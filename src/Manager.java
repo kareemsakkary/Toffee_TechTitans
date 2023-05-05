92073c01131a16637765ff7db98c07d66174f54e
Snowkaai <mohamed.waleed1023@gmail.com>
2023-05-05 21:02:49 +0300

    Manager class

---
+++ src/Manager.java
@@ -0,0 +1,9 @@
+import OrderManagement.*;
+import StockManagement.*;
+import Authentication.*;
+
+public class Manager {
+    private OrderManagement ordManager;
+    private StockManagement stckManager;
+    private Authentication auth;
+}
