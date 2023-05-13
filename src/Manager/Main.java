044acb619af06d5f7f78ad23d98bff68f7e28be4
Snowkaai <mohamed.waleed1023@gmail.com>
2023-05-13 11:21:23 +0300

    main refactor + package info

--- src/Manager/Main.java
+++ src/Manager/Main.java
@@ -1,9 +1,14 @@
-import Manager.Manager;
+package Manager;
+
 
 /**
  *  Main class that runs the program.
  */
 public class Main {
+    /**
+     *  Method that runs the program.
+     * @param args
+     */
     public static void main(String[] args) {
         Manager M = new Manager();
         M.run();
